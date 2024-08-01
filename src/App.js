4080a56c289060ebb3aac24cc1a3cbae3c24a249
D-Limiter <>
2024-08-01 11:06:57 +0800

    Initialise project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build the MERN TINDER clone guys!!</h1>
+      <Header />
+      {/* Header */}
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
